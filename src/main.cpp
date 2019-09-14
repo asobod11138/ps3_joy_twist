4b1a2f545d766486496e19733d2c8c6757492636
mano <mano@aisl.cs.tut.ac.jp>
2019-09-14 17:25:50 +0900

    [init]

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+
+geometry_msgs::Twist cmd_vel;
+void joy_callback(const sensor_msgs::Joy& joy_msg) {
+  cmd_vel.linear.x = joy_msg.axes[1];
+  cmd_vel.linear.y = joy_msg.axes[0];
+  cmd_vel.linear.z = 3*float(joy_msg.buttons[0]);
+  cmd_vel.angular.z = 90*joy_msg.axes[3];
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "basic_twist_publisher");
+  ros::NodeHandle n;
+
+  // publish
+  ros::Publisher cmd_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+
+  // subscriibe
+  ros::Subscriber joy_sub = n.subscribe("joy", 10, joy_callback);
+
+  ros::Rate loop_rate(10);
+  while (ros::ok()) {
+    cmd_pub.publish(cmd_vel);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
+
